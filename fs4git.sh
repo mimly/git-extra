d3d032ca04753d8c76135b3c85851373532813a2
mimly <m.j.mlynczak@gmail.com>
2020-10-05 12:25:23 +0200

    Update prompt script
    Signed-off-by: mimly <m.j.mlynczak@gmail.com>

--- fs4git.sh
+++ fs4git.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+gitInfo() {
+    local usedColorSequences=6
+    # Each color is 13 characters long
+    local bColor="[1;38;5;046m"
+    local cColor="[1;38;5;240m"
+
+    local branch=`branch 2>/dev/null`
+    local hash=`commitHash 1 2>/dev/null`
+    local message=`commitMessage $hash 2>/dev/null`
+
+    # Shorten hash and message if too long
+    local gitStatus=`git status --porcelain 2>/dev/null`
+    if [[ -z $gitStatus ]] ; then
+        hash="[1;38;5;045m#$cColor${hash:0:4}[1;38;5;046m✔$cColor"
+    else
+        hash="[1;38;5;045m#$cColor${hash:0:4}[1;38;5;196m✘$cColor"
+    fi
+    if [[ ${#message} -gt 10 ]] ; then
+        message="${message:0:10}..."
+    fi
+
+    # In order to wrap lines correctly
+    if [[ $# -eq 1 ]] ; then
+        local gitInfo=`gitInfo`
+        for (( i = 0; i < ${#gitInfo} - 13 * $usedColorSequences; ++i )) ; do
+            printf "%s" ""
+        done
+        exit 0
+    fi
+
+    if ! [[ -z $branch ]] ; then
+        printf "${bColor}on %s $cColor[%s %s] " "$branch" "$hash" "$message"
+    fi
+}
+
+branch() {
+    git branch | awk '{if ($1 == "*") { $1 = ""; print $0 }}'
+}
+
 isInvalidDate() {
   date "+%FT%T" -d "$1" 2>&1 >/dev/null
   if [[ $? != 0 ]] ; then
