096d4110de59abc59420b805e130f25c8a20fdae
mimly <m.j.mlynczak@gmail.com>
2020-10-07 17:20:52 +0200

    Lint and add some minor changes
    Signed-off-by: mimly <m.j.mlynczak@gmail.com>

--- fs4git.sh
+++ fs4git.sh
@@ -6,12 +6,16 @@ gitInfo() {
     local bColor="[1;38;5;046m"
     local cColor="[1;38;5;240m"
 
-    local branch=`branch 2>/dev/null`
-    local hash=`commitHash 1 2>/dev/null`
-    local message=`commitMessage $hash 2>/dev/null`
+    local branch
+    branch=$(branch 2>/dev/null)
+    local hash
+    hash=$(commitHash 1 2>/dev/null)
+    local message
+    message=$(commitMessage "$hash" 2>/dev/null)
 
     # Shorten hash and message if too long
-    local gitStatus=`git status --porcelain 2>/dev/null`
+    local gitStatus
+    gitStatus=$(git status --porcelain 2>/dev/null)
     if [[ -z $gitStatus ]] ; then
         hash="[1;38;5;045m#$cColor${hash:0:4}[1;38;5;046m✔$cColor"
     else
@@ -23,14 +27,15 @@ gitInfo() {
 
     # In order to wrap lines correctly
     if [[ $# -eq 1 ]] ; then
-        local gitInfo=`gitInfo`
-        for (( i = 0; i < ${#gitInfo} - 13 * $usedColorSequences; ++i )) ; do
+        local gitInfo
+        gitInfo=$(gitInfo)
+        for (( i = 0; i < ${#gitInfo} - 13 * usedColorSequences; ++i )) ; do
             printf "%s" ""
         done
         exit 0
     fi
 
-    if ! [[ -z $branch ]] ; then
+    if [[ -n $branch ]] ; then
         printf "${bColor}on %s $cColor[%s %s] " "$branch" "$hash" "$message"
     fi
 }
@@ -39,16 +44,15 @@ branch() {
     git branch | awk '{if ($1 == "*") { $1 = ""; print $0 }}'
 }
 
-isInvalidDate() {
-  date "+%FT%T" -d "$1" 2>&1 >/dev/null
-  if [[ $? != 0 ]] ; then
-      return 0 
-  fi
-  return 1
+isValidDate() {
+  local date=$1
+  date "+%FT%T" -d "$date" >/dev/null 2>&1
+  local isValidDate=$?
+  [[ $isValidDate -eq 0 && ${#date} -eq 19 ]]
 }
 
 totalNumberOfCommits() {
-    git rev-list --count $1
+    git rev-list --count "$1"
 }
 
 commitHash() {
@@ -56,9 +60,9 @@ commitHash() {
 }
 
 commitMessage() {
-    git rev-list --format=%s --max-count=1 $1 | awk '{if (NR == 2) { print $0 }}'
+    git rev-list --format=%s --max-count=1 "$1" | awk '{if (NR == 2) { print $0 }}'
 }
 
 commitDate() {
-    git rev-list --format=%ci --max-count=1 $1 | awk '{if (NR == 2) { print $0 }}'
+    git rev-list --format=%ci --max-count=1 "$1" | awk '{if (NR == 2) { print $0 }}'
 }
