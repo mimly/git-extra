283eb45a9c56df9b9715141a83a8f3d2c419569b
mimly <m.j.mlynczak@gmail.com>
2021-09-19 02:28:41 +0200

    Refactor main scripts and allow saving current changes into past revisions (PART II)
    Signed-off-by: mimly <m.j.mlynczak@gmail.com>

--- fs4git.sh
+++ fs4git.sh
@@ -8,15 +8,15 @@ gitBranchInfo() {
         local branch
         branch=$(branch 2>/dev/null)
         local gitBranchInfo
-        gitBranchInfo=$(colorize --fg-color 50 --fg-step 4 "on  $branch")
+        gitBranchInfo=$(colorize --fg-color 50 --fg-step 4 "on  ${branch}")
 
         allCharacters=$(( ${#gitBranchInfo} + 1 ))
-        nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "$x") ))
-        printf "%s " "$gitBranchInfo"
+        nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "${x}") ))
+        printf "%s " "${gitBranchInfo}"
     fi
 
-    echo $allCharacters >&7
-    echo $nonPrintableCharacters >&7
+    echo "${allCharacters}" >&7
+    echo "${nonPrintableCharacters}" >&7
 }
 
 gitCommitInfo() {
@@ -40,7 +40,7 @@ gitCommitInfo() {
         local statusColor
         local gitStatus
         gitStatus=$(git status --porcelain)
-        if [[ -z $gitStatus ]] ; then
+        if [[ -z ${gitStatus} ]] ; then
             status="✔"
             statusColor=46
         else
@@ -49,15 +49,15 @@ gitCommitInfo() {
         fi
 
         local gitCommitInfo
-        gitCommitInfo=$(colorize --fg-style 3 --fg-custom-scheme 0:238,1:$hashColor,2:239,4:240,6:$statusColor,7:241,8:242,10:243,12:244,14:245,16:246,18:247,20:248,22:249,24:250,26:251 "[#$hash$status $message]")
+        gitCommitInfo=$(colorize --fg-style 3 --fg-custom-scheme "0:238,1:${hashColor},2:239,4:240,6:${statusColor},7:241,8:242,10:243,12:244,14:245,16:246,18:247,20:248,22:249,24:250,26:251" "[#${hash}${status} ${message}]")
 
         allCharacters=$(( ${#gitCommitInfo} + 1 ))
-        nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "$x") ))
-        printf "%s " "$gitCommitInfo"
+        nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "${x}") ))
+        printf "%s " "${gitCommitInfo}"
     fi
 
-    echo $allCharacters >&7
-    echo $nonPrintableCharacters >&7
+    echo "${allCharacters}" >&7
+    echo "${nonPrintableCharacters}" >&7
 }
 
 gitPushInfo() {
@@ -68,15 +68,15 @@ gitPushInfo() {
         local branch
         branch=$(branch 2>/dev/null)
         local gitPushInfo
-        gitPushInfo=$(colorize --fg-color 50 --fg-step 4 "➤ origin/$branch")
+        gitPushInfo=$(colorize --fg-color 50 --fg-step 4 "➤ origin/${branch}")
 
         allCharacters=$(( ${#gitPushInfo} + 1 ))
-        nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "$x") ))
-        printf "%s " "$gitPushInfo"
+        nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "${x}") ))
+        printf "%s " "${gitPushInfo}"
     fi
 
-    echo $allCharacters >&7
-    echo $nonPrintableCharacters >&7
+    echo "${allCharacters}" >&7
+    echo "${nonPrintableCharacters}" >&7
 }
 
 gitSubmoduleInfo() {
@@ -87,31 +87,31 @@ gitSubmoduleInfo() {
         local submodules
         submodules=$(git config --file .gitmodules --get-regexp path | awk '{ print $2 }' ORS=' ')
         local submodulesArray
-        IFS=' ' read -ra submodulesArray <<< "$submodules"
+        IFS=' ' read -ra submodulesArray <<< "${submodules}"
         local gitSubmoduleInfo
-        gitSubmoduleInfo=$(colorize --fg-color 238 --fg-color-step 1 "🡶 ")
-        nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "$x") ))
+        gitSubmoduleInfo=$(colorize --fg-color 238 --fg-color-step 1 "⚜ ")
+        nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "${x}") ))
         for (( i = 0; i < "${#submodulesArray[@]}"; ++i )) ; do
             local gitStatus
-            gitStatus=$(git --git-dir "${submodulesArray[$i]}/.git" --work-tree "${submodulesArray[$i]}" status --porcelain 2>/dev/null)
-            if [[ -n $gitStatus ]] ; then
+            gitStatus=$(git --git-dir "${submodulesArray[${i}]}/.git" --work-tree "${submodulesArray[${i}]}" status --porcelain 2>/dev/null)
+            if [[ -n ${gitStatus} ]] ; then
                 color=$(( 196 + i ))
             else
                 color=$(( 238 + i * 3 ))
             fi
 
-            gitSubmoduleInfo+=$(colorize --fg-color "$color" --fg-color-step 1 "${submodulesArray[$i]}")
-            nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "$x") ))
+            gitSubmoduleInfo+=$(colorize --fg-color "${color}" --fg-color-step 1 "${submodulesArray[${i}]}")
+            nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "${x}") ))
 
-            if [[ $i -ne $(( ${#submodulesArray[@]} - 1 )) ]] ; then
+            if [[ ${i} -ne $(( ${#submodulesArray[@]} - 1 )) ]] ; then
                 gitSubmoduleInfo+=$(colorize --fg-color "$(( 238 + i * 3 ))" --fg-color-step 1 "|")
-                nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "$x") ))
+                nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "${x}") ))
             fi
         done
-        gitSubmoduleInfo+=$(colorize --fg-color "$(( 238 + ( ${#submodulesArray[@]} - 1 ) * 3 ))" --fg-color-step 1 " 🡶")
+        gitSubmoduleInfo+=$(colorize --fg-color "$(( 238 + ( ${#submodulesArray[@]} - 1 ) * 3 ))" --fg-color-step 1 " ⚜")
         allCharacters=$(( ${#gitSubmoduleInfo} + 1 ))
-        nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "$x") ))
-        printf "%s " "$gitSubmoduleInfo"
+        nonPrintableCharacters=$(( nonPrintableCharacters + $(read -r -u 7 x ; echo "${x}") ))
+        printf "%s " "${gitSubmoduleInfo}"
 
         # A little bit simpler approach
         #gitSubmoduleInfo=$(colorize --fg-color 238 --fg-color-step 1 "🡶 $(IFS='|' ; echo "${submodulesArray[*]}") 🡶")
@@ -121,8 +121,8 @@ gitSubmoduleInfo() {
         #printf "%s " "$gitSubmoduleInfo"
     fi
 
-    echo $allCharacters >&7
-    echo $nonPrintableCharacters >&7
+    echo "${allCharacters}" >&7
+    echo "${nonPrintableCharacters}" >&7
 }
 
 isGitRepo() {
@@ -132,19 +132,19 @@ isGitRepo() {
 hasAnySubmodules() {
     local submodules
     submodules=$(git config --file .gitmodules --get-regexp path)
-    [[ -n $submodules ]]
+    [[ -n ${submodules} ]]
 }
 
 isRemoteSetUp() {
     local remote
     remote=$(git remote -v 2>/dev/null)
-    [[ -n $remote ]]
+    [[ -n ${remote} ]]
 }
 
 isPushed() {
     local branch
     branch=$(branch 2>/dev/null)
-    [[ $(commitHash "$branch") == $(commitHash "origin/$branch") ]]
+    [[ $(commitHash "${branch}") == $(commitHash "origin/${branch}") ]]
 }
 
 branch() {
@@ -153,51 +153,128 @@ branch() {
 
 isValidDate() {
     local date=$1
-    date +%Y-%m-%dT%H:%M:%S --date="$date" >/dev/null 2>&1 && [[ $date =~ ^.{10}T.{8}$ ]]
+    date +%Y-%m-%dT%H:%M:%S --date="${date}" >/dev/null 2>&1 && [[ ${date} =~ ^.{10}T.{8}$ ]]
 }
 
+### TODO Test how the functions work outside of a git repository.
+
+##################################################
+# Returns the total number of commits. Depends on the commitHash function.
+#
+# Globals:
+#   None
+# Arguments:
+#   $1 - commit number or commit hash
+# Outputs:
+#   Writes the number of commits (or nothing at all) to stdout
+#   and returns always exit code 0.
+##################################################
 totalNumberOfCommits() {
-    local REVISION
-    REVISION=$(commitHash "$1")
-    git rev-list --count "${REVISION:-0}" 2>/dev/null
+    local hash=$1
+    hash=$(commitHash "${hash}")
+    git rev-list --count "${hash}" 2>/dev/null || return 0
 }
 
+##################################################
+# Returns the number of commits between any two commits. Depends on the commitHash function.
+#
+# Globals:
+#   None
+# Arguments:
+#   $1 - commit number or commit hash
+#   $2 - commit number or commit hash
+# Outputs:
+#   Writes the number of commits between (or nothing at all) to stdout
+#   and returns always exit code 0.
+##################################################
 numberOfCommitsBetween() {
-    local FROM TO NUMBER
-    FROM=$(commitHash "$1")
-    TO=$(commitHash "$2")
-    NUMBER=$(git rev-list --count "${FROM:-0}..${TO:-0}" 2>/dev/null)
-    if [[ $NUMBER -eq 0 ]] ; then
-        git rev-list --count "${TO:-0}..${FROM:-0}" 2>/dev/null
-    else
-        echo "$NUMBER"
+    local from to number
+    from=$(commitNumber "$(commitHash "$1")")
+    to=$(commitNumber "$(commitHash "$2")")
+    number=$(( to - from ))
+    if [[ -n ${from} && -n ${to} ]] ; then
+        printf "%s\n" "${number#-}" # abs
     fi
 }
 
+##################################################
+# Checks whether the specified commit is valid or not. Depends on the commitHash function.
+#
+# Globals:
+#   None
+# Arguments:
+#   $1 - commit number or commit hash
+# Outputs:
+#   Returns exit code 0 if the specified commit is valid and exit code 1 otherwise.
+##################################################
 isValidCommit() {
-    commitHash "$1" >/dev/null 2>&1
+    [[ -n $(commitHash "$1") ]]
 }
 
+##################################################
+# Returns the hash value of the specified commit.
+#
+# Globals:
+#   None
+# Arguments:
+#   $1 - commit number or commit hash
+# Outputs:
+#   Writes commit hash (or nothing at all) to stdout
+#   and returns always exit code 0.
+##################################################
 commitHash() {
-    if [[ $1 =~ ^[1-9][0-9]*$ && $1 -le $(totalNumberOfCommits HEAD) ]] ; then
-        git rev-parse --verify --quiet HEAD~$(( $1 - 1 )) 2>/dev/null
-    else
-        git rev-parse --verify --quiet "$1" 2>/dev/null
+    local hash=$1
+    if [[ ${hash} =~ ^[1-9][0-9]*$ ]] && (( hash <= $(totalNumberOfCommits HEAD) )) ; then
+        hash=HEAD~$(( hash - 1 ))
     fi
+    git rev-parse --verify --quiet "${hash}" 2>/dev/null || true
 }
 
+##################################################
+# Returns the order number of the specified commit. Depends on the commitHash function.
+#
+# Globals:
+#   None
+# Arguments:
+#   $1 - commit number or commit hash
+# Outputs:
+#   Writes commit number (or nothing at all) to stdout
+#   and returns always exit code 0.
+##################################################
 commitNumber() {
     git rev-list --count HEAD~$(( $(totalNumberOfCommits "$(commitHash "$1")") - 1 )) 2>/dev/null
 }
 
+##################################################
+# Returns the message of the specified commit. Depends on the commitHash function.
+#
+# Globals:
+#   None
+# Arguments:
+#   $1 - commit number or commit hash
+# Outputs:
+#   Writes commit message (or nothing at all) to stdout
+#   and returns always exit code 0.
+##################################################
 commitMessage() {
     git rev-list --format=%s --max-count=1 "$(commitHash "$1")" 2>/dev/null | awk '{if (NR == 2) { print $0 }}'
 }
 
+##################################################
+# Returns the date of the specified commit. Depends on the commitHash function.
+#
+# Globals:
+#   None
+# Arguments:
+#   $1 - commit number or commit hash
+# Outputs:
+#   Writes commit date (or nothing at all) to stdout
+#   and returns always exit code 0.
+##################################################
 commitDate() {
-    local commitDate
-    commitDate=$(git rev-list --format=%ci --max-count=1 "$(commitHash "$1")" 2>/dev/null | awk '{if (NR == 2) { print $0 }}')
-    if [[ -n $commitDate ]] ; then
-        date +%Y-%m-%dT%H:%M:%S --date="$commitDate"
+    local date
+    date=$(git rev-list --format=%ci --max-count=1 "$(commitHash "$1")" 2>/dev/null | awk '{if (NR == 2) { print $0 }}')
+    if [[ -n ${date} ]] ; then
+        date +%Y-%m-%dT%H:%M:%S --date="${date}"
     fi
 }
